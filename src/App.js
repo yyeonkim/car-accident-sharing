3ae6a94c6931ef13a061bc288e42e816bbbd1356
yyeonkim <seyeon.dev@gmail.com>
2022-08-20 11:07:38 +0900

    Remove unused codes

--- src/App.js
+++ src/App.js
@@ -2,29 +2,6 @@ import { Center, Flex, Heading } from "@chakra-ui/react";
 
 import VideoList from "./components/VideoList";
 
-const videos = [
-  {
-    id: "32874824",
-    thumbnail:
-      "https://images.unsplash.com/photo-1566933293069-b55c7f326dd4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-  },
-  {
-    id: "234",
-    thumbnail:
-      "https://images.unsplash.com/photo-1566933293069-b55c7f326dd4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-  },
-  {
-    id: "43434",
-    thumbnail:
-      "https://images.unsplash.com/photo-1566933293069-b55c7f326dd4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-  },
-  {
-    id: "234432",
-    thumbnail:
-      "https://images.unsplash.com/photo-1566933293069-b55c7f326dd4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-  },
-];
-
 export default function App() {
   return (
     <Center py="6rem">
