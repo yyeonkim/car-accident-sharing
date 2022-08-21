a25231c5279c0f57e7180d133ca1614ba6ee473e
kimwest00 <kimwest00@hufs.ac.kr>
2022-08-21 09:16:32 +0900

    FEAT - firebase 서버 install

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import "@fontsource/roboto";
 import React from "react";
+
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-
+import firebase from './Firebase'
 import { RecoilRoot } from "recoil";
 import Router from "./routes/Router";
 import { customeTheme } from "./theme";
-
+console.log(firebase);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
