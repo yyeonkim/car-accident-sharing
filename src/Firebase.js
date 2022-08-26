3d14e3889a70e34851b3400fd2ca97aece6677e7
Yongyeon Kim <yeonlotus@ewhain.net>
2022-08-26 15:11:27 +0900

    docs: Add env file

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,17 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLE3UFuCgdGSxRMJbcNd02whpcPMcLSpE",
-  authDomain: "gongcha-2b0f0.firebaseapp.com",
-  projectId: "gongcha-2b0f0",
-  storageBucket: "gongcha-2b0f0.appspot.com",
-  messagingSenderId: "349778674",
-  appId: "1:349778674:web:6a6bc4544c2858703f966b",
-  measurementId: "G-WXYJJYCCZ0",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export default app;
+export const db = getFirestore(app);
