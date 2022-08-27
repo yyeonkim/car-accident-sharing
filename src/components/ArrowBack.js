0be0bd0dfcfc6efd4864c868282114fc56548779
Yongyeon Kim <yeonlotus@ewhain.net>
2022-08-27 15:38:01 +0900

    feat: Add ArrowBackIcon component

---
+++ src/components/ArrowBack.js
@@ -0,0 +1,16 @@
+import { ArrowBackIcon } from "@chakra-ui/icons";
+import { useNavigate } from "react-router-dom";
+
+export default function ArrowBack() {
+  const navigate = useNavigate();
+
+  return (
+    <ArrowBackIcon
+      cursor="pointer"
+      w={8}
+      h={8}
+      mt="2rem"
+      onClick={() => navigate(-1)}
+    />
+  );
+}
