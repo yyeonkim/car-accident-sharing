331ab22f20eda4dcfec0e81b52d023be1980e72d
Yongyeon Kim <yeonlotus@ewhain.net>
2022-08-28 12:50:31 +0900

    feat: Get results having comments from db

---
+++ src/hooks/useGetResults.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+
+import { db } from "../firebase";
+
+export default function useGetResults() {
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      let items = [];
+      const querySnapshot = await getDocs(collection(db, "videos"));
+      querySnapshot.forEach((doc) => {
+        if (Object.keys(doc.data().comments).length !== 0) {
+          items.push(doc.data());
+        }
+      });
+      setResults(items);
+      setIsLoading(false);
+    })();
+  }, []);
+
+  return { results, isLoading };
+}
--- src/routes/ResultList.js
+++ src/routes/ResultList.js
@@ -4,27 +4,44 @@ import { Link } from "react-router-dom";
 
 import Result from "../components/Result";
 import ArrowBack from "../components/ArrowBack";
+import LoadingAnimation from "../components/LoadingAnimation";
+import useGetResults from "../hooks/useGetResults";
 import { myVideos } from "../db";
 
 export default function ResultList() {
+  const { results, isLoading } = useGetResults();
+
   return (
     <Flex direction="column" mx="2rem">
-      <ArrowBack url="/user" />
+      {isLoading ? (
+        <LoadingAnimation />
+      ) : (
+        <>
+          <ArrowBack url="/user" />
 
-      <Heading fontSize="xl" lineHeight="2rem" my="1rem">
-        <Text fontSize="lg">김공차님</Text>
-        <Text display="flex">
-          총 &nbsp;<Text color="blue">3</Text>건의 분석 결과 입니다.
-        </Text>
-      </Heading>
+          <Heading fontSize="xl" lineHeight="2rem" my="1rem">
+            <Text fontSize="lg">김공차님</Text>
+            <Text display="flex">
+              총 &nbsp;
+              <Text color="blue">{myVideos.length + results.length}</Text>건의
+              분석 결과 입니다.
+            </Text>
+          </Heading>
 
-      <Stack spacing="1rem">
-        {myVideos.map((item) => (
-          <Link to={item.id} key={item.id}>
-            <Result item={item} />
-          </Link>
-        ))}
-      </Stack>
+          <Stack spacing="1rem">
+            {myVideos.map((item) => (
+              <Link to={item.id} key={item.id}>
+                <Result item={item} />
+              </Link>
+            ))}
+            {results.map((item) => (
+              <Link to={item.id} key={item.id}>
+                <Result item={item} />
+              </Link>
+            ))}
+          </Stack>
+        </>
+      )}
     </Flex>
   );
 }
