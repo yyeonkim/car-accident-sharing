e92d199be144bd6d81ff7d41eaa2bd11651ea135
Yongyeon Kim <yeonlotus@ewhain.net>
2022-08-30 11:47:30 +0900

    docs: Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  authDomain: "gongcha-2b0f0.firebaseapp.com",
+  projectId: "gongcha-2b0f0",
+  storageBucket: "gongcha-2b0f0.appspot.com",
+  messagingSenderId: "349778674",
+  appId: "1:349778674:web:6a6bc4544c2858703f966b",
+  measurementId: "G-WXYJJYCCZ0",
 };
 
 // Initialize Firebase
